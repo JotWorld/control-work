081c9cb39969846f20179091f76d21f77b01b9b3
JotWorld <lewenka2004@gmail.com>
2025-05-16 06:24:30 +0300

    feat:added basic functionality of buttons

--- src/model/films-model.js
+++ src/model/films-model.js
@@ -6,8 +6,36 @@ export default class FilmsModel {
     { id: '4', title: 'BB', watched: true }
   ];
 
+  #observers = new Set();
+
   get films() {
     return this.#films;
   }
-  
+
+  addFilm(title, watched) {
+    const newFilm = {
+      id: String(Date.now()),
+      title,
+      watched
+    };
+    this.#films.push(newFilm);
+    this.#notify();
+  }
+
+  deleteFilmById(id) {
+    this.#films = this.#films.filter(film => film.id !== id);
+    this.#notify();
+  }
+
+  addObserver(observer) {
+    this.#observers.add(observer);
+  }
+
+  removeObserver(observer) {
+    this.#observers.delete(observer);
+  }
+
+  #notify() {
+    this.#observers.forEach((observer) => observer());
+  }
 }
